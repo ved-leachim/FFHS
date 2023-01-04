251b494fbe88e8004d2e3a44f69ea7bb0eed427a
Michael Schmitz <michael.schmitz@deepsphere.ch>
2023-01-04 01:03:32 +0100

    Implement a Stack Class
    - Implement a Custom Stack Class
    - This Stack uses an Array as fundamental DS
    - The Stack starts with a size of 5 and can grow until 10

--- Datastructures and Algorithms/Datastructures/Linear DS/Stack/src/main/java/Main.java
+++ Datastructures and Algorithms/Datastructures/Linear DS/Stack/src/main/java/Main.java
@@ -1,19 +1,40 @@
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        /* String Reverser
+        // String Reverser
         String string = "abcd";
 
         var reverser = new StringReverser();
         var result = reverser.reverse(string);
 
         System.out.println(result);
-        */
 
         // BalancedBracketsChecker
         String str = "(1(2[a[b{c{d<ee<ff>gg>hh}11}22]33]44)5!)";
         var bracketsChecker = new BalancedBracketsChecker();
         System.out.println(bracketsChecker.isBalanced(str));
+
+        // Custom Stack Class
+        var numberStack = new Stack();
+        numberStack.push(10);
+        numberStack.push(20);
+        System.out.println(numberStack);
+        System.out.println("Peek: " + numberStack.peek());
+        System.out.println("IsEmpty: " + numberStack.isEmpty());
+        System.out.println("Pop: " + numberStack.pop());
+        System.out.println("Peek: " + numberStack.peek());
+        System.out.println("Pop: " + numberStack.pop());
+        System.out.println("IsEmpty: " + numberStack.isEmpty());
+        numberStack.push(10);
+        numberStack.push(20);
+        numberStack.push(30);
+        numberStack.push(40);
+        numberStack.push(50);
+        numberStack.push(60);
+        numberStack.push(70);
+        numberStack.push(80);
+        numberStack.push(90);
+        numberStack.push(100);
+        System.out.println(numberStack);
+        System.out.println("Peek: " + numberStack.peek());
     }
 }
--- Datastructures and Algorithms/Datastructures/Linear DS/Stack/src/main/java/Stack.java
+++ Datastructures and Algorithms/Datastructures/Linear DS/Stack/src/main/java/Stack.java
@@ -1,2 +1,52 @@
+import java.util.Arrays;
+
 public class Stack {
+    private int[] intArray = new int[5];
+    private int count = 0;
+
+    @Override
+    public String toString() {
+        var content = Arrays.copyOfRange(intArray, 0, count);
+        return Arrays.toString(content);
+    }
+
+    public void push(int number) {
+        // Max size of the dynamic Stack is 10
+        if (count == 10)
+            throw new StackOverflowError();
+        if (isStackFull())
+            growArray();
+
+        intArray[count++] = number;
+    }
+
+    public int pop() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return intArray[--count];
+    }
+
+    public int peek() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return intArray[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    private boolean isStackFull() {
+        return count == intArray.length;
+    }
+
+    private void growArray() {
+        var copiedArray = new int[(count) * 2];
+
+        for (int i = 0; i < count; i++)
+            copiedArray[i] = intArray[i];
+        intArray = copiedArray;
+    }
 }
