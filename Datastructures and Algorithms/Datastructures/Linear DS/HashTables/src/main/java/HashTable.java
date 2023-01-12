194ff4d5ebbfa64a058d8eec35944ba52e4a7ddb
Michael Schmitz <michael.schmitz@deepsphere.ch>
2023-01-12 21:08:12 +0100

    Implement HashTable
    - Implement a custom HashTable with the function: put(), get() and remove()
    - The HashTable should accept int as keys and Strings as values

---
+++ Datastructures and Algorithms/Datastructures/Linear DS/HashTables/src/main/java/HashTable.java
@@ -0,0 +1,87 @@
+import java.security.Key;
+import java.util.LinkedList;
+
+/**
+ * Exercise: Build a HashTable from scratch, that contains:
+ *  - put (k, v)
+ *  - get (k): v
+ *  - remove(k)
+ *
+ *  k: int
+ *  v: string
+ *
+ *  Collisions handling with chaining (Linked Lists)
+ *  Hints:  - The basic Array DS saves Linked Lists
+ *          - Create a separate class for key and value to be stored in
+ */
+public class HashTable {
+
+    private LinkedList<KeyValuePair>[] entries = new LinkedList[5];
+
+    public void put(int key, String value) {
+        var keyValuePair = new KeyValuePair(key, value);
+
+        var savingIndex = hash(key);
+        if (!hasCollision(savingIndex))
+            saveToHashTable(savingIndex, keyValuePair);
+        else {
+            var ll = entries[savingIndex];
+            var kvp = getKeyValuePair(key, ll);
+            if (kvp != null)
+                kvp.value = keyValuePair.value;
+            else
+                ll.add(keyValuePair);
+        }
+    }
+
+    private KeyValuePair getKeyValuePair(int key, LinkedList<KeyValuePair> ll) {
+        for (int i = 0; i < ll.size(); i++) {
+            var currentKeyValuePair = ll.get(i);
+            if (key == currentKeyValuePair.key)
+                return currentKeyValuePair;
+        }
+        return null;
+    }
+
+    private void saveToHashTable (int index, KeyValuePair kvp) {
+        var ll = new LinkedList<KeyValuePair>();
+        ll.add(kvp);
+        entries[index] = ll;
+    }
+
+    private boolean hasCollision(int savingIndex) {
+        return entries[savingIndex] != null;
+    }
+
+    public String get(int key) {
+        var savingIndex = hash(key);
+        var ll = entries[savingIndex];
+        var kvp = getKeyValuePair(key, ll);
+        if (kvp == null)
+            return "The key " + key + " does not exist in the HashTable.";
+        return kvp.value;
+    }
+
+    public void remove(int key) {
+        var savingIndex = hash(key);
+        var ll = entries[savingIndex];
+        var kvp = getKeyValuePair(key, ll);
+        ll.remove(kvp);
+    }
+
+    private int hash(int key) {
+        String getHashValue = "" + key;
+        int hash = getHashValue.hashCode();
+        return ((hash % 5) + 5) % 5;
+    }
+
+    private class KeyValuePair {
+        private int key;
+        private String value;
+
+        public KeyValuePair(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
--- Datastructures and Algorithms/Datastructures/Linear DS/HashTables/src/main/java/Main.java
+++ Datastructures and Algorithms/Datastructures/Linear DS/HashTables/src/main/java/Main.java
@@ -3,9 +3,24 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Programming is awesome";
-        var charFinder = new CharFinder();
-        var character = charFinder.findFirstRepeatingChar(str);
-        System.out.println(character);
+        var ht = new HashTable();
+        ht.put(0, "Value 1");
+        ht.put(1, "Value 2");
+        ht.put(2, "Value 3");
+        ht.put(3, "Value 4");
+        ht.put(4, "Value 5");
+        ht.put(5, "Value 6");
+        String val1 = ht.get(0);
+        System.out.println(val1);
+        ht.put(0, "Value 11");
+        val1 = ht.get(0);
+        String val2 = ht.get(1);
+        String val5 = ht.get(5);
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val5);
+        ht.remove(0);
+        val1 = ht.get(0);
+        System.out.println(val1);
     }
 }
